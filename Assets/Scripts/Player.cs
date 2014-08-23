54a9afc075f002dc8d62a04ef25ef5bc8bfd5efd
Chris Wade <cjacobwade@gmail.com>
2014-08-22 20:57:00 -0500

    Whoopsie commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	[SerializeField] float moveSpeed;
+
+	Vector3 lastPosition;
+	Vector3 moveVector;
+
+	void Awake()
+	{
+
+	}
+
+	void Update()
+	{
+		transform.position += moveVector;
+
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			moveVector = Vector2.up;
+		}
+		else if(Input.GetKeyDown(KeyCode.S))
+		{
+			moveVector = -Vector2.up;
+		}
+		else if(Input.GetKeyDown(KeyCode.A))
+		{
+			moveVector = -Vector2.right;
+		}
+		else if(Input.GetKeyDown(KeyCode.D))
+		{
+			moveVector = Vector2.right;
+		}
+	}
+
+	void LateUpdate()
+	{
+		lastPosition = transform.position;
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		transform.position = lastPosition;
+	}
+}
