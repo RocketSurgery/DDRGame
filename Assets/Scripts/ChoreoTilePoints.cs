1869d7c3aeec8839419df48964558a9586dd2b0a
David LeGare <excaliburHisSheath@gmail.com>
2014-08-23 10:30:12 -0500

    Flip Tile Skeleton - Basic flip tile implementation based off of the jump tile. When the player is over it and is pressing the correct key combo, it disappears and flips the worlds.

--- Assets/Scripts/ChoreoTilePoints.cs
+++ Assets/Scripts/ChoreoTilePoints.cs
@@ -8,7 +8,7 @@ public class ChoreoTilePoints : MonoBehaviour {
     public float threshhold = 0.2f;
 
 	// Use this for initialization
-	void Start () {
+	protected void Start () {
         scoreManagerObject = GameObject.Find("ScoreManager");
         scoreManagerComponent = scoreManagerObject.GetComponent<ScoreManager>();
 	}
