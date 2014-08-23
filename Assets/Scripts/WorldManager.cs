a4d5698a3e5bc882c94edcde97998ad57b1bbebe
David LeGare <excaliburHisSheath@gmail.com>
2014-08-22 21:23:14 -0500

    Basic World Flipping Implementation work on #3 - WorldManager has 2 parent objects for each world. - WorldManager can toggle which world is active.

---
+++ Assets/Scripts/WorldManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour {
+
+	public GameObject world1;
+	public GameObject world2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			world1.SetActive(!world1.activeSelf);
+			world2.SetActive(!world2.activeSelf);
+		}
+	}
+}
