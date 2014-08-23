f853ef088c3f29395924c54a4ab5eb99751197b3
Zac Gross <zac.a.gross@gmail.com>
2014-08-23 14:41:09 -0500

    Lines up tiles and implements first pass at grid snap

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,27 +32,44 @@ public class Player : MonoBehaviour
 		}
 	}
 
+    public float distanceToNearestPosition;
+    public Vector3 nearestPosition;
+    public Vector3 debugPos;
+
     void Update()
     {
+        debugPos = transform.position;
+        nearestPosition = new Vector3( Mathf.RoundToInt(transform.position.x),Mathf.RoundToInt(transform.position.y), 0);
+
+        distanceToNearestPosition = Vector3.Project(transform.position - nearestPosition, moveVector).magnitude;
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
+        {            
             moveVector = Vector2.up;
             transform.transform.rotation = Quaternion.Euler(Vector3.zero);
+            Vector3 moveVector3 = new Vector3(moveVector.x, moveVector.y, 0);
+            transform.position = nearestPosition + distanceToNearestPosition * moveVector3;
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             moveVector = -Vector2.up;
             transform.transform.rotation = Quaternion.Euler(Vector3.forward * 180.0f);
+            Vector3 moveVector3 = new Vector3(moveVector.x, moveVector.y, 0);
+            transform.position = nearestPosition + distanceToNearestPosition * moveVector3;
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             moveVector = -Vector2.right;
             transform.transform.rotation = Quaternion.Euler(Vector3.forward * 90.0f);
+            Vector3 moveVector3 = new Vector3(moveVector.x, moveVector.y, 0);
+            transform.position = nearestPosition + distanceToNearestPosition * moveVector3;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             moveVector = Vector2.right;
             transform.transform.rotation = Quaternion.Euler(Vector3.forward * -90.0f);
+            Vector3 moveVector3 = new Vector3(moveVector.x, moveVector.y, 0);
+            transform.position = nearestPosition + distanceToNearestPosition * moveVector3;
         }
     }
 }
