ddb899f0c9e26672337cffff6e0f81b78f78e14d
David LeGare <excaliburHisSheath@gmail.com>
2014-08-23 08:32:37 -0500

    Player Flipping with World Flipping - Player model is now flipped when worlds are flipped.

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -5,11 +5,16 @@ public class WorldManager : MonoBehaviour
 {
 	public GameObject world1;
 	public GameObject world2;
+	public GameObject playerModel1;
+	public GameObject playerModel2;
 
 	public void FlipWorlds()
 	{
 		world1.SetActive(!world1.activeSelf);
 		world2.SetActive(!world2.activeSelf);
+
+		playerModel1.SetActive(!playerModel1.activeSelf);
+		playerModel2.SetActive(!playerModel2.activeSelf);
 	}
 
 	// Update is called once per frame
