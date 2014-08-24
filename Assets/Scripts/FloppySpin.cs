d1d9db635186b8883372d9e1548b0b2d1627f06b
David LeGare <excaliburHisSheath@gmail.com>
2014-08-24 11:07:44 -0500

    Floppy Spin - Floppies now spin in a very 90's fashion.

---
+++ Assets/Scripts/FloppySpin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloppySpin : MonoBehaviour
+{
+	void Update ()
+	{
+		transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), -90.0f * Time.deltaTime, Space.World);
+	}
+}
