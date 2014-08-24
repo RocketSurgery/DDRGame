3aa9f1954e2926d232b3fe5e5384873da3716df1
David LeGare <excaliburHisSheath@gmail.com>
2014-08-24 14:21:56 -0500

    HUD Skeleton work on #22 - Basic HUD shows score and time remaining in level. - Fixes camera follow so that it's not broken by moving the camera's starting position.

---
+++ Assets/Scripts/UI/TimeScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeScript : MonoBehaviour
+{
+	TimeManager timeManager;
+	Text text;
+	string baseText;
+
+	void Start()
+	{
+		text = GetComponent<Text>();
+		baseText = text.text;
+		timeManager = GameObject.Find("TimeManager").GetComponent<TimeManager>();
+	}
+
+	void Update()
+	{
+		text.text = baseText + timeManager.Time;
+	}
+}
