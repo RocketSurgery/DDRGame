e54683eb9dc6b52d07a89054d13dc4a77716dffb
Chris Wade <cjacobwade@gmail.com>
2014-08-25 02:41:41 -0500

    Singleton change

---
+++ Assets/Scripts/CollectibleTile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleTile : MonoBehaviour
+{
+	void OnTriggerStay2D()
+	{
+		ScoreManager.singleton.instance.AddScore();
+		ItemText.singleton.instance.DisplayText("You have stolen some government secrets!");
+		Destroy(gameObject);
+	}
+}
